7ef4ddd1e3fb25111ca92ba527839759fd1e4d2a
NGO THE DAT <188000162+Ngo-The-Dat@users.noreply.github.com>
2025-11-14 13:41:16 +0700

    change the file path to the backend folder

--- backend/src/routes/productRoutes.js
+++ backend/src/routes/productRoutes.js
@@ -1,4 +1,4 @@
-import { get_all_products, add_product, delete_product, update_product, get_all_NXB, get_all_TACGIA } from '../controllers/productController.js';
+import { get_all_products, add_product, delete_product, update_product, get_all_NXB, get_all_TACGIA, get_product_by_id } from '../controllers/productController.js';
 import express from 'express';
 import { protect, restrictTo } from '../controllers/authMiddleware.js';
 
@@ -7,6 +7,7 @@ const router = express.Router();
 router.get('/get_all_products', protect, restrictTo('admin'), get_all_products);
 router.post('/add_product', protect, restrictTo('admin'), add_product);
 router.delete('/delete_product/:id', protect, restrictTo('admin'), delete_product);
+router.get('/:id', protect, restrictTo('admin'), get_product_by_id)
 router.put('/update_product/:id', protect, restrictTo('admin'), update_product);
 router.get('/NXB', protect, restrictTo('admin'), get_all_NXB);
 router.get('/TACGIA', protect, restrictTo('admin'), get_all_TACGIA)
