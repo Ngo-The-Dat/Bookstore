849b4b3360a13dc353cc9580fda4af3fde9599b1
Le Thanh Vinh <60224102+awookaze@users.noreply.github.com>
2025-11-14 16:54:18 +0700

    Add LoginPage and Toaster to App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,22 @@
-import {Toaster, toast} from 'sonner';
-import {BrowserRouter, Routes, Route} from 'react-router';
-import HomePage from './pages/HomePage';
-import NotFound from './pages/NotFound';
-
+import { Toaster } from "sonner";
+import { BrowserRouter, Routes, Route } from "react-router";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
+import LoginPage from "./pages/LoginPage";
 
 function App() {
-
-  return <>
-    <BrowserRouter>
-      <Routes>
-
-        <Route
-          path='/'
-          element = {<HomePage />}
-        />
-
-        <Route
-          path='*'
-          element = {<NotFound />}
-        />
-
-      </Routes>
-    </BrowserRouter>
-  </>;
+  return (
+    <>
+      <BrowserRouter>
+        <Toaster richColors position="top-right" />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
