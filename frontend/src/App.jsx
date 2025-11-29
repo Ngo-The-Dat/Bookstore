05d2e5821b9d0e8aab205626671ebcffec8e8e99
Le Thanh Vinh <60224102+awookaze@users.noreply.github.com>
2025-11-29 21:42:16 +0700

    Add RegisterPage and ChatBubble components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
 import LoginPage from "./pages/LoginPage";
-import BookDetail from "./pages/BookDetail";
+import RegisterPage from "./pages/RegisterPage";
+import ChatBubble from "./components/ChatBubble";
 
 function App() {
+  console.log("Current API URL:", import.meta.env.VITE_API_URL);
   return (
     <>
       <BrowserRouter>
         <Toaster richColors position="top-right" />
         <Routes>
-          <Route path="/book/:id" element={<BookDetail />} />
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
+        <ChatBubble />
       </BrowserRouter>
     </>
   );
