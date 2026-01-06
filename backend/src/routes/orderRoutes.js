bd62177058ca99668abd2e0663fa4e9988b95417
Le Thanh Vinh <60224102+awookaze@users.noreply.github.com>
2026-01-06 14:18:39 +0700

    Add route to get all orders for admin

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import { createOrder, getOrderHistory, getOrderById, updateOrderStatus, cancelOrder } from "../controllers/orderController.js"
-import { protect } from "../middlewares/auth.js"
+import { createOrder, getOrderHistory, getOrderById, updateOrderStatus, cancelOrder, getAllOrders } from "../controllers/orderController.js"
+import { protect, restrictTo } from "../middlewares/auth.js"
 
 const router = express.Router()
 
+router.get("/all", protect, restrictTo('admin'), getAllOrders) // Admin: Lấy tất cả đơn hàng
 router.post("/", protect, createOrder) // Tạo đơn hàng
 router.get("/history", protect, getOrderHistory) // Lấy lịch sử đơn hàng của người dùng
 router.get("/:orderId", protect, getOrderById) // Lấy chi tiết 1 đơn hàng
